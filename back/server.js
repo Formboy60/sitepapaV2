50f32474558501724b60cfdfa0c97dd1d58a24d3
formboy60 <formboy@gmail.com>
2022-06-03 18:46:02 +0200

    commit

---
+++ back/server.js
@@ -0,0 +1,21 @@
+const express = require ('express')
+const mongoose = require("mongoose")
+const { use } = require('./route.js')
+require ('dotenv').config()
+const Router = require("./route.js")
+const app = express()
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(`${process.env.MONGOOSE}`)
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+
+
+
+app.use(Router);
+app.listen(PORT)
